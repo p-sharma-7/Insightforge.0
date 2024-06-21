33e7a302e621488c11669f2a6fa8189bd12ac3ee
p-sharma-7 <puskarrokhel@gmail.com>
2024-06-21 23:23:12 +0530

    Fix encoding issue in requirements.txt and update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,13 @@
-from setuptools import find_packages, setup
-from typing import List
+from setuptools import setup, find_packages
 
-Hyphen_E_dot='-e .'
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function reads the requirements file and returns the list of requirements
-    '''
-    requirements=[]
-    with open (file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements= [req.replace("\n","")for req in requirements]
+def get_requirements(filename):
+    with open(filename, encoding='utf-8') as file_obj:
+        requirements = file_obj.readlines()
+    return [req.strip() for req in requirements]
 
-        if Hyphen_E_dot in requirements:
-            requirements.remove(Hyphen_E_dot)
-    return requirements
-    
 setup(
-name='Insightforge',
-version='0.0.1',
-author='Pushkar Sharma',
-author_email= 'pushkarrokhel@gmail.com',
-packages=find_packages(),
-requirements = get_requirements('requirements.txt')
+    name="Insightforge",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
 )
