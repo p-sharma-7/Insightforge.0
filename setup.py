6cfca25320c311af3622deed8d229d0e6548bf62
Pushkar Sharma <152854842+p-sharma-7@users.noreply.github.com>
2024-09-08 16:28:01 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Insightforge.0",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        "streamlit",
+        "pandas",
+        "scikit-learn",
+        ﻿"scipy"
+        "matplotlib"
+        "numpy"
+        "setuptools"
+        "streamlit"
+        "duckdb"
+        "openpyxl"
+        "lazypredict"
+        "category_encoders"
+        "statsmodels"
+        "matplotlib"
+        "scikit-learn"
+        "catboost"
+        "xgboost"
+        "streamlit_lottie
+        "html5lib"
+        "lxml"
+        "dill"
+        # Add any other dependencies from your requirements.txt
+    ],
+)
